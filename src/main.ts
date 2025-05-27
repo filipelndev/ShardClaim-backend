8699ca0569a469f8ad4cb489d225148597951bf3
Filipe Noronha <98480552+filipelndev@users.noreply.github.com>
2025-05-27 16:25:40 -0300

    test cors 3
    test cors 3

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.enableCors({
-  origin: ['https://brazildh-filipelndevs-projects.vercel.app'],
+  origin: ['https://brazildh.vercel.app/'],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Permite cookies e tokens de autenticação
